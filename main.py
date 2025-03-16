049b00bc393b8fc1c99db1391e534e328f2b0273
fengnightstarts <98741472+fengnightstarts@users.noreply.github.com>
2025-03-16 17:15:00 +0800

    试验提交

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import PySide6.QtWidgets
 from queue import Queue
 import threading
-from view import login_view, main_view, log_view
-from log import log_to_window
+import view
+import log
 import app.main
+import view.log_view
 
 
 def main():
     q = Queue()
     ap = PySide6.QtWidgets.QApplication()
-    login_window = login_view.login_window()
-    main_window = main_view.main_window()
-    log_window = log_view.log_window()
-    log_to_window.init(log_window)
+    login_window = view.login_view.login_window()
+    main_window = view.main_view.main_window()
+    log_window = view.log_view.log_window()
+    log.log_to_window.init(log_window)
     log_window.show()
     t = threading.Thread(
         target=app.main.start,
